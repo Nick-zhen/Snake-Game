efcf30b659c5e69200767e07dbd91bebeab70eb1
knzheng <knzheng@student.ubc.ca>
2022-08-18 14:55:29 -0700

    update

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,14 @@
 
 int main()
 {
+    // implement in stack
     Game game;
-    game.Run();
+    game.run();
+    // implement in heap;
+    // keyword new will return a pointer
+    // Game* game = new Game();
+    // game->Run();
+    // delete game;
 
     return 0;
 }
